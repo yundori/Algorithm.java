4e2ff92bb41f5221e3a3b3e812c50c6f8bb9dc1a
yundori <yundori22@gmail.com>
2022-12-19 15:38:45 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 132 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String args[]) throws IOException {
+
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+          int nowSum = 0;
+          int[] array = new int[9];
+
+          for(int i = 0; i < 9; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            array[i] = Integer.parseInt(st.nextToken());
+            nowSum += array[i];
+          }
+          
+          for(int i = 0; i < 8; i++){
+            for(int j = i+1; j < 9; j++){
+                if(nowSum - array[i] - array[j] == 100){
+                    array[i] = 0;
+                    array[j] = 0;
+                    Arrays.sort(array);
+                    for(int k = 2; k < 9; k++){
+                        System.out.println(array[k]);
+                    }
+                    return;
+                }
+            }
+          }
+    }
+}
